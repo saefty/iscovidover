42ff3ca5cd48de29d60cb404677078ec8f7e9c82
Saef Taher <saef.taher@dkbcodefactory.com>
2021-02-01 18:51:02 +0100

    Init first build

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,9 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <nav sx={{ variant: 'layout.nav' }}>
-      <div style={{position: "absolute", right: 0}}>
+      <div style={{position: "absolute", right: 0, cursor: 'pointer', height: '64px', width: '64px', margin: '10px'}}>
 				<Link href="https://github.com/saefty/iscovidover">
-					<Image src="/GitHub-Mark-120px-plus.png" width={64} height={64}/>
+					<Image src="/GitHub-Mark-Light-120px-plus.png" layout='fill'/>
         </Link>
       </div>
     </nav>
